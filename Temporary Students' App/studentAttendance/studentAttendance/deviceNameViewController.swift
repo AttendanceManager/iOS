d9a0023265a087562ef79bc7805396005ceac4c4
Shivanshu Chaudhary <shivanshuchaudhary97@gmail.com>
2017-11-12 11:08:07 +0530

    no message

--- Temporary Students' App/studentAttendance/studentAttendance/deviceNameViewController.swift
+++ Temporary Students' App/studentAttendance/studentAttendance/deviceNameViewController.swift
@@ -33,9 +33,77 @@ class deviceNameViewController: UIViewController {
 
     
     func generateDeviceName(){
-        print(userEnteredCode)
         
-        generatedDeviceName = userEnteredCode
+        var passcode = userEnteredCode
+        
+        var place1Dict = [  "0":"1",
+                            "1":"2",
+                            "2":"3",
+                            "3":"4",
+                            "4":"5",
+                            "5":"6",
+                            "6":"7",
+                            "7":"8",
+                            "8":"9",
+                            "9":":"
+        ]
+        var place2Dict = [  "0":".",
+                            "1":"/",
+                            "2":"0",
+                            "3":"1",
+                            "4":"2",
+                            "5":"3",
+                            "6":"4",
+                            "7":"5",
+                            "8":"6",
+                            "9":"7"
+        ]
+        var place3Dict = [  "0":"3",
+                            "1":"4",
+                            "2":"5",
+                            "3":"6",
+                            "4":"7",
+                            "5":"8",
+                            "6":"9",
+                            "7":":",
+                            "8":";",
+                            "9":"<"
+        ]
+        var place4Dict = [  "0":",",
+                            "1":"-",
+                            "2":".",
+                            "3":"/",
+                            "4":"0",
+                            "5":"1",
+                            "6":"2",
+                            "7":"3",
+                            "8":"4",
+                            "9":"5"
+        ]
+        
+        
+        var deviceName = ""
+        
+        deviceName += ("!!"+userRollNumber+"#V#")
+        
+        var count = 1
+        
+        for chrc in passcode.characters{
+            if(count == 1){
+                deviceName += (place1Dict[String(chrc)]!)
+            }else if(count == 2){
+                deviceName += (place2Dict[String(chrc)]!)
+            }else if(count == 3){
+                deviceName += (place3Dict[String(chrc)]!)
+            }else if(count == 4){
+                deviceName += (place4Dict[String(chrc)]!)
+            }
+            count += 1
+        }
+        
+        deviceName += "#"
+        
+        generatedDeviceName = deviceName
     }
     
     
